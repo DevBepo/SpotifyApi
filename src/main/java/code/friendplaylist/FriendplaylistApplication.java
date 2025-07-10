3541685db9390518565fe4e29de70c478246a0e5
Jonas Lima <Jonaslimadomingues@gmail.com>
2025-07-10 05:00:05 -0300

    Project created

---
+++ src/main/java/code/friendplaylist/FriendplaylistApplication.java
@@ -0,0 +1,13 @@
+package code.friendplaylist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FriendplaylistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FriendplaylistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/code/friendplaylist/FriendplaylistApplicationTests.java
@@ -0,0 +1,13 @@
+package code.friendplaylist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FriendplaylistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
